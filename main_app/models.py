bfdd2be27401f55a0f57a95dcedc64c08d93fd8d
Rachel Carleton <rachel.carleton@icloud.com>
2024-01-10 08:11:08 -0800

    part two

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book (models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000)
+    publishedyear =  models.IntegerField()
+    def __str__(self):
+        return self.title
